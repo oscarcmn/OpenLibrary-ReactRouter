6a6b549ba1dac713d0aec213cdf42fd67fe48c63
wafaes <148322310+wafaes@users.noreply.github.com>
2025-03-11 18:10:51 +0100

    Update TopBar.tsx

--- app/components/TopBar/TopBar.tsx
+++ app/components/TopBar/TopBar.tsx
@@ -2,26 +2,35 @@ import { Link } from "react-router";
 
 function TopBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-red-500 to-yellow-500 shadow-md p-4 flex justify-between items-center text-white">
-      {/* Logo / Title */}
-      <Link
-        to="/"
-        className="flex items-center gap-2 text-2xl font-bold tracking-wide hover:text-yellow-200 transition"
-      >
-        <img src="/logobien.png" alt="Poke-App Logo" className="h-8 w-8" />
-        Poke-App
-      </Link>
+    <div className="container z-100">
+      <nav className="fixed top-0 left-0 w-full bg-[var(--color-soft-yellow)] shadow-md flex justify-between items-center text-black p-4">
+        {/* Logo / Title */}
+        <Link to="/search" className="flex items-center gap-2 text-2xl font-bold tracking-wide hover:text-green-700 transition">
+          <img src="/bookie-logo.png" alt="Bookie Logo" className="w-18" />
+        </Link>
 
-      {/* Navigation Links */}
-      <ul className="flex space-x-6">
-        <li>
-          <Link to="favorites">Favorites</Link>
-        </li>
-        <li>
-          <Link to="about">About</Link>
-        </li>
-      </ul>
-    </nav>
+        {    }
+        <ul className="flex space-x-6">
+           <li>
+            <Link to="/favorites">
+              Favourites
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/search">
+              Home
+            </Link>
+          </li>
+          
+         
+        </ul>
+      </nav>
+    </div>
   );
 }
 
