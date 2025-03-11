c2bd531f1c5e74122176e25991d94a1962ca9dab
ocarmena <oscarcarmenaprofesional@gmail.com>
2025-03-11 20:18:26 +0100

    details acabado

--- app/components/BookDetails/BookDetails.tsx
+++ app/components/BookDetails/BookDetails.tsx
@@ -8,14 +8,20 @@ interface BookDetailsProps {
 function BookDetails({ bookdetails }: BookDetailsProps) {
   const covers = bookdetails.covers || [];
   return (
-    <div className="text-black">
-      <h1>{bookdetails.title}</h1>
-      <img
-        src={`https://covers.openlibrary.org/b/id/${covers[0] || ""}.jpg`}
-        className="img-top w-40 h-40 object-contain"
-        alt="Book cover"
-      />
-      <p>{bookdetails.description}</p>
+    <div className="text-black p-4 ml-20 mr-20 mt-10">
+      <div className="flex flex-col md:flex-row">
+        <img
+          src={`https://covers.openlibrary.org/b/id/${covers[0] || ""}.jpg`}
+          className="w-40 h-71 object-cover rounded-lg mb-4 md:mb-0 md:mr-4"
+          alt="Book cover"
+        />
+
+        <div>
+          <h1 className="text-2xl font-bold mb-4">{bookdetails.title}</h1>
+          <h2 className="text-xl font-bold mb-4">Sinopsis</h2>
+          <p className="mt-2">{bookdetails.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
