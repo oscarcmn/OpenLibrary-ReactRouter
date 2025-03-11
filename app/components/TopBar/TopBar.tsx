b770813476b15a0521d8cae2ba79abb5a4df432a
ocarmena <oscarcarmenaprofesional@gmail.com>
2025-03-11 18:31:32 +0100

    - errores

--- app/components/TopBar/TopBar.tsx
+++ app/components/TopBar/TopBar.tsx
@@ -5,29 +5,24 @@ function TopBar() {
     <div className="container z-100">
       <nav className="fixed top-0 left-0 w-full bg-[var(--color-soft-yellow)] shadow-md flex justify-between items-center text-black p-4">
         {/* Logo / Title */}
-        <Link to="/search" className="flex items-center gap-2 text-2xl font-bold tracking-wide hover:text-green-700 transition">
+        <Link
+          to=""
+          className="flex items-center gap-2 text-2xl font-bold tracking-wide hover:text-green-700 transition"
+        >
           <img src="/bookie-logo.png" alt="Bookie Logo" className="w-18" />
         </Link>
 
-        {    }
+        {}
         <ul className="flex space-x-6">
-           <li>
-            <Link to="/favorites">
-              Favourites
-            </Link>
+          <li>
+            <Link to="/favorites">Favourites</Link>
           </li>
           <li>
-            <Link to="/about">
-              About
-            </Link>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/search">
-              Home
-            </Link>
+            <Link to="">Home</Link>
           </li>
-          
-         
         </ul>
       </nav>
     </div>
